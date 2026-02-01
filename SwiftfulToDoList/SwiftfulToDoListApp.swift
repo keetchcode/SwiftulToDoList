67ab61f9e5865666b05302ca2f6435c2c783b312
Wesley Keetch <keetchcode@gmail.com>
2026-02-01 13:55:48 -0500

    Initial Commit

---
+++ SwiftfulToDoList/SwiftfulToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulToDoListApp.swift
+//  SwiftfulToDoList
+//
+//  Created by Wesley Keetch on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
